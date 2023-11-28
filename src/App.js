7bb4b66e18da045f25f0439ce45d63270b582150
k-sugawa <sugawakumiko@gmail.com>
2023-11-28 20:20:08 +0900

    残りのタスク数を出す

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import TodoList from "./TodoList";
 import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-  const [todos, setTodos] = useState([
-    { id: 1, name: "Todo1", completed: false },
-  ]);
+  const [todos, setTodos] = useState([]);
 
   const todoNameRef = useRef();
 
@@ -31,7 +29,7 @@ function App() {
       <button>完了したタスクの削除</button>
       <TodoList todos={todos} toggleTodo={toggleTodo}/>
       
-      <div>残りのタスク: 0</div>
+      <div>残りのタスク: {todos.filter((todo) => !todo.completed).length}</div>
     </>
   );
 }
