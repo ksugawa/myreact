121d919bc3f5cf0dec40b1f2ffe882723146ab23
k-sugawa <sugawakumiko@gmail.com>
2023-11-28 17:59:40 +0900

    setting todo place

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import "./App.css"
+import { useState } from "react";
+import TodoList from "./TodoList";
 
 function App() {
-  return <div className="App">Hello React</div>
+  const [ todos, setTodos ] = useState();
+  return (
+    <>
+      <TodoList todos={todos} />
+      <input type="text" />
+      <button>タスクを追加</button>
+      <button>完了したタスクの削除</button>
+      <div>残りのタスク: 0</div>
+    </>
+  );
 }
 
 export default App;
